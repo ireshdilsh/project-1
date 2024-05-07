2947519a7e3545aa3c91f0eaa682d3e0d688a7ad
Iresh Dilshan <ireshdilshan46@gmail.com>
2024-05-07 20:36:12 +0530

    add headerpage

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import { Box, Grid } from '@mui/material'
+import React from 'react'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+
+function Header() {
+  return (
+    <div>
+    
+        
+
+    </div>
+  )
+}
+
+export default Header
