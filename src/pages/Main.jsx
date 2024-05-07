e02a11bf253fec91b822fbfabaf0586e15a0e8d6
Iresh Dilshan <ireshdilshan46@gmail.com>
2024-05-07 19:44:45 +0530

    update

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Button from '@mui/material/Button';
+
+function Main() {
+
+  return (
+    <div>
+         
+         <Button variant="outlined">Outlined</Button>
+
+    </div>
+  )
+}
+
+export default Main
